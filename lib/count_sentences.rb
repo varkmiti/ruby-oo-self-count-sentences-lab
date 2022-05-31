889195433e6b4811115756bea121affe33196bf0
Mark Viti <+>
2022-05-31 13:41:08 -0400

    commit tree 846d1aaf9035aa7aca90f2f5a778dda64f3b01f8

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,38 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1, 1] == "." 
+      return true
+    else 
+      return false
+    end 
   end
 
   def question?
-
+    if self[-1,1] == "?"
+      return true 
+    else 
+      return false
+    end
   end
 
   def exclamation?
-
+    if self[-1,1] == "!"
+      return true
+    else 
+      return false
+    end 
   end
 
   def count_sentences
-
+    no_comma = self.gsub(/[,]/, "")
+    nospace = no_comma.gsub(/[ ]/, "")
+    sentences= nospace.split(/(?<=[.])/)
+    sentences.count
   end
+
+  # def count_sentences
+  #   no_comma = self.gsub(/[,]/, "")
+
+  # end
 end
